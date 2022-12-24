8b46791ccd51ce3e08a351fcc1f6338c4729cec6
Antonio Gravino <19vienna20@gmail.com>
2022-12-24 15:32:49 +0100

    Remove local data; update Python routines

--- __http/app.py
+++ __http/app.py
@@ -1,6 +1,33 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+
+from flask import Flask, render_template
+from pathlib import Path
+import signal
+from subprocess import Popen, PIPE
+from flask_cors import CORS
+
+app = Flask(__name__, template_folder='template')
+CORS(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    dir = str(Path('__http/template/index.html').parent.absolute())
+    return render_template('index.html')
+
+@app.route('/ras')
+def ras():
+    dir = str(Path('__http/template/ras.html').parent.absolute())
+    return render_template('ras.html')
+
+@app.route('/patient')
+def patient():
+    dir = str(Path('__http/template/patient.html').parent.absolute())
+    return render_template('patient.html')
+
+@app.route('/run/<command>')
+def run(command):
+    dir = str(Path('__twin/examples/' + command + '.py').parent.absolute())
+    path = str(dir) + '/' + command + '.py'
+    print(path)
+    out = os.popen('python3.7 ' + path).read()
+    return render_template('index.html')
